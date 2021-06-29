46959c1cd31246e93c8c477a6ddcc36734450807
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-29 12:29:00 +0900

    feat :: 저장되어 있는 학생 리스트 받아오기

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StudentList from "./Components/StudentList";
 import io from "socket.io-client";
+import axios from "axios";
 
 const PORT = process.env.PORT || 4000;
 const socket = io.connect(`http://localhost:${PORT}`);
 
 const App = () => {
-	const [studentList, setStudentList] = useState([
-		"박시연",
-		"김정원",
-		"손형탁",
-	]);
+	const [studentList, setStudentList] = useState([]);
 	const [inputState, setInputState] = useState("");
+	const [state, setState] = useState([]);
 	const myName = "박시연";
 
 	const isFirstYourName = () => {
 		return myName === studentList[0] ? true : false;
 	};
+	useEffect(() => {
+		async function fetchData() {
+			await axios
+				.get("http://localhost:4000")
+				.then((res) => {
+					setState(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		let tempStudentList = [];
+
+		for (let student of state) {
+			tempStudentList.push(student.stdName);
+		}
+
+		setStudentList([...tempStudentList]);
+	}, [state]);
 
 	const handleCreate = () => {
 		let newStdList = studentList;
